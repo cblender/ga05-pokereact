5ca2aadec40f0dc6a422312f0f1c789efb4bdb3f
cblender <christopher.blender@gmail.com>
2020-08-28 11:08:31 -0400

    big copy over from old broken repo. http://cblender.github.io/ga05-react-ui <--URL

--- src/App.js
+++ src/App.js
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+/*$$$$$$$           /$$                /$$ /$$$$$$$                                  /$$    
+| $$__  $$         | $$               | $/| $$__  $$                                | $$    
+| $$  \ $$ /$$$$$$ | $$   /$$  /$$$$$$|_/ | $$  \ $$  /$$$$$$   /$$$$$$   /$$$$$$$ /$$$$$$  
+| $$$$$$$//$$__  $$| $$  /$$/ /$$__  $$   | $$$$$$$/ /$$__  $$ |____  $$ /$$_____/|_  $$_/  
+| $$____/| $$  \ $$| $$$$$$/ | $$$$$$$$   | $$__  $$| $$$$$$$$  /$$$$$$$| $$        | $$    
+| $$     | $$  | $$| $$_  $$ | $$_____/   | $$  \ $$| $$_____/ /$$__  $$| $$        | $$ /$$
+| $$     |  $$$$$$/| $$ \  $$|  $$$$$$$   | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/
+|__/      \______/ |__/  \__/ \_______/   |__/  |__/ \_______/ \_______/ \_______/   \___*/
+                                                                                            
+import React, { Component } from 'react';
 import './App.css';
+import Pokemon from './Pokemon';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      pokeList: []
+    }
+    this.pokeQuantity = 151
+    this.pokeGeneration = 1
+    this.pokeList = []
+  }
+
+  
+
+  fetchPokeList = () => {
+    fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
+    .then(response => response.json())
+    .then((allpokemon) => {
+      
+      console.log("========================================")
+      console.log(allpokemon)
+      console.log("========================================")
+      allpokemon.results.forEach((pokemon) => {
+        let pokeList = this.state.pokeList
+        pokeList.push(pokemon)
+        this.setState({pokeList})
+        // console.log(pokemon)
+      })
+    })
+    // console.log(this.pokeList)
+  }
+
+  componentDidMount () {
+    this.fetchPokeList();
+  }
+
+  render () {
+
+    return (
+      <div className="appContainerDoNotTouch">
+        <div>
+          <h1>PokeReact</h1>
+        </div>
+        <div className="spacer">
+          <p>a React UI by Chris Blendermann</p>
+        </div>
+        <div className="container">
+          {this.state.pokeList.map(
+            (pokemon, i) => {
+              return (<Pokemon key={i} name={pokemon.name} url={pokemon.url} />)
+            }
+          )}  
+        </div>
+
+        <div className="pokedex" style={{display:'none', color:'white', background:'black'}} >
+                    <p>Pokedex Placeholder Text</p>
+                    
+                </div>
+      </div> // END DIV CLASS "appContainerDoNotTouch".
+    )
+  }
 }
 
 export default App;
---
+++ src/Pokemon.js
@@ -0,0 +1,114 @@
+import React, { Component } from 'react';
+import './Pokemon.css';
+
+class Pokemon extends Component {
+    
+    constructor(props) {
+        super(props);
+        this.render = this.render.bind(this);
+        this.state = {
+            name: this.props.name,
+            url: this.props.url,
+            pokeInfo: '',
+            showPokedex: false
+        };
+
+        // console.log(this.state)
+    }
+
+    componentDidMount = () => {
+        // console.log("FIRED! componentDidMount for "+this.state.name)
+
+        let url = this.state.url
+
+        fetch(url)
+        .then(response => response.json())
+        .then((pokeData) => {
+            let pokeInfo = this.state.pokeInfo
+            pokeInfo = pokeData
+            this.setState({pokeInfo})
+
+            let imgsrc = this.state.pokeInfo.sprites.other.dream_world.front_default
+            this.setState({img: imgsrc})
+        })
+
+        
+    }    
+
+    handlePokedex = () => {
+        console.log("FIRED! handlePokedex for "+this.state.pokeInfo.name)
+        console.log(this.state.pokeInfo, this.state.showPokedex)
+        // console.log(this.state.pokeInfo.sprites.other.official-artwork.front_default)
+        if (!this.state.showPokedex) {
+            let bool = true
+            this.setState({showPokedex: bool})
+        }
+        else {
+            let bool = false
+            this.setState({showPokedex: bool})
+        }
+        
+        // THE GREAT POKEMON INFORMATION TRAWLER
+
+
+        let statXP = this.state.pokeInfo.base_experience
+        let statHP = this.state.pokeInfo.stats[0].base_stat
+        let statATK = this.state.pokeInfo.stats[1].base_stat
+        let statDEF = this.state.pokeInfo.stats[2].base_stat
+        let statSPD = this.state.pokeInfo.stats[5].base_stat
+        let statWeight = this.state.pokeInfo.weight
+
+        this.setState({XP: statXP})
+        this.setState({HP: statHP})
+        this.setState({ATK: statATK})
+        this.setState({DEF: statDEF})
+        this.setState({SPD: statSPD})
+        this.setState({WEI: statWeight})
+
+    }
+
+    render = () => {
+        let pokedex
+        let show = this.state.showPokedex
+        let backgroundStyle = {backgroundImage:"url("+this.state.img+")", backgroundSize:"contain", backgroundPosition:"center center", backgroundRepeat:"no-repeat"}
+
+        if (show) {
+            pokedex = 
+                <div className="pokedex">
+                    <h1>{this.state.pokeInfo.name}</h1>
+                    <img src={this.state.img}></img>
+                    <h3>{this.state.pokeInfo.types[0].type.name}</h3>
+                    <div>
+                        <h4>    
+                            Base XP: {this.state.XP} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; 
+                            HP: {this.state.HP} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            Atack: {this.state.ATK} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            Defense: {this.state.DEF} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            Speed: {this.state.SPD} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                            Weight: {this.state.WEI}
+                        </h4>
+                    </div>
+                </div>
+                
+        }
+        else {
+            pokedex = <div style={{display:'none'}}></div>
+        }
+
+        return(
+            <div className="pokeContainerDoNotTouch" onClick={this.handlePokedex}>
+ 
+                <div className="container" style={backgroundStyle}>
+                    <h3 className="pokeTitle">{this.props.name}</h3>
+                </div>
+
+                <div className="pokedexContainer">
+                    {pokedex}
+                </div>
+
+            </div>
+        )
+    }
+}
+
+export default Pokemon;
