8129e9ef9dc88b9ceb34bfc79279812d4c01bd04
cblender <christopher.blender@gmail.com>
2020-10-09 20:06:55 -0400

    improved modal styling.  ready for animations.

--- src/App.js
+++ src/App.js
@@ -6,86 +6,83 @@
 | $$     | $$  | $$| $$_  $$ | $$_____/   | $$  \ $$| $$_____/ /$$__  $$| $$        | $$ /$$
 | $$     |  $$$$$$/| $$ \  $$|  $$$$$$$   | $$  | $$|  $$$$$$$|  $$$$$$$|  $$$$$$$  |  $$$$/
 |__/      \______/ |__/  \__/ \_______/   |__/  |__/ \_______/ \_______/ \_______/   \___*/
-                                                                                            
-import React, { Component } from 'react';
-import './App.css';
-import Pokemon from './Pokemon';
+
+import React, { Component } from "react";
+import "./App.css";
+import Pokemon from "./Pokemon";
+import Navbar from "./Navbar";
 
 class App extends Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      pokeList: []
-    }
-    this.pokeQuantity = 151
-    this.pokeGeneration = 1
-    this.pokeList = []
+      pokeList: [],
+    };
+    this.pokeQuantity = 151;
+    this.pokeGeneration = 1;
+    this.pokeList = [];
   }
-
-  
 
   fetchPokeList = () => {
     fetch("https://pokeapi.co/api/v2/pokemon?limit=151")
-    .then(response => response.json())
-    .then((allpokemon) => {
-      
-      console.log("========================================")
-      console.log(allpokemon)
-      console.log("========================================")
-      allpokemon.results.forEach((pokemon) => {
-        let pokeList = this.state.pokeList
-        pokeList.push(pokemon)
-        this.setState({pokeList})
-        // console.log(pokemon)
-      })
-    })
+      .then((response) => response.json())
+      .then((allpokemon) => {
+        console.log("========================================");
+        console.log(allpokemon);
+        console.log("========================================");
+        allpokemon.results.forEach((pokemon) => {
+          let pokeList = this.state.pokeList;
+          pokeList.push(pokemon);
+          this.setState({ pokeList });
+          // console.log(pokemon)
+        });
+      });
     // console.log(this.pokeList)
-  }
+  };
 
-  componentDidMount () {
+  componentDidMount() {
     this.fetchPokeList();
   }
 
-  render () {
-
+  render() {
     return (
       <div className="appContainerDoNotTouch">
-
-        <div className="headBack">
-
-        </div>
+        <div className="headBack"></div>
 
         <div className="headTitle">
           <h1>PokeReact</h1>
           <h4>a React UI by Chris Blendermann</h4>
         </div>
 
-        <div className="headSpace">
-          
-        </div>
-        
+        <div className="headSpace"></div>
+
         <div className="flexbox">
           <div className="container">
-            {this.state.pokeList.map(
-              (pokemon, i) => {
-                return (<Pokemon key={i} name={pokemon.name} url={pokemon.url} />)
-              }
-            )}  
+            {this.state.pokeList.map((pokemon, i) => {
+              return <Pokemon key={i} name={pokemon.name} url={pokemon.url} />;
+            })}
           </div>
         </div>
 
         <div className="headLogo">
-          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c23eacf8-e366-4118-ab5c-5a1255842a75/de44djs-8ffd142e-adc7-4602-9b2c-fd4e3f09e926.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzIzZWFjZjgtZTM2Ni00MTE4LWFiNWMtNWExMjU1ODQyYTc1XC9kZTQ0ZGpzLThmZmQxNDJlLWFkYzctNDYwMi05YjJjLWZkNGUzZjA5ZTkyNi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.a9Q1Y1a02P0ibUAjC4DhAHuLQlwZH-Wzxyfs-oBnJHE"
-          alt="Pokeball Logo" width="200px" height="200px"></img>
+          <img
+            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c23eacf8-e366-4118-ab5c-5a1255842a75/de44djs-8ffd142e-adc7-4602-9b2c-fd4e3f09e926.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzIzZWFjZjgtZTM2Ni00MTE4LWFiNWMtNWExMjU1ODQyYTc1XC9kZTQ0ZGpzLThmZmQxNDJlLWFkYzctNDYwMi05YjJjLWZkNGUzZjA5ZTkyNi5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.a9Q1Y1a02P0ibUAjC4DhAHuLQlwZH-Wzxyfs-oBnJHE"
+            alt="Pokeball Logo"
+            width="200px"
+            height="200px"
+          ></img>
         </div>
 
         <div className="buttMenu">
-          <img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c23eacf8-e366-4118-ab5c-5a1255842a75/de44dio-c44bc257-f323-4270-8985-9d001473ccbd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzIzZWFjZjgtZTM2Ni00MTE4LWFiNWMtNWExMjU1ODQyYTc1XC9kZTQ0ZGlvLWM0NGJjMjU3LWYzMjMtNDI3MC04OTg1LTlkMDAxNDczY2NiZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.7d4Fg-kB1tH2DqZqm-Al4OCChqvWwMPDXB-T064CMso"
-          alt="Menu Button" width="69px" height="69px"></img>
+          <img
+            src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c23eacf8-e366-4118-ab5c-5a1255842a75/de44dio-c44bc257-f323-4270-8985-9d001473ccbd.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzIzZWFjZjgtZTM2Ni00MTE4LWFiNWMtNWExMjU1ODQyYTc1XC9kZTQ0ZGlvLWM0NGJjMjU3LWYzMjMtNDI3MC04OTg1LTlkMDAxNDczY2NiZC5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.7d4Fg-kB1tH2DqZqm-Al4OCChqvWwMPDXB-T064CMso"
+            alt="Menu Button"
+            width="69px"
+            height="69px"
+          ></img>
         </div>
-
       </div> // END DIV CLASS "appContainerDoNotTouch".
-    )
+    );
   }
 }
 
--- src/Pokemon.js
+++ src/Pokemon.js
@@ -1,147 +1,184 @@
-import React, { Component } from 'react';
-import './Pokemon.css';
+import React, { Component } from "react";
+import "./Pokemon.css";
 
 class Pokemon extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.render = this.render.bind(this);
-        this.state = {
-            name: this.props.name,
-            url: this.props.url,
-            pokeInfo: '',
-            showPokedex: false
-        };
+  constructor(props) {
+    super(props);
+    this.render = this.render.bind(this);
+    this.state = {
+      name: this.props.name,
+      url: this.props.url,
+      pokeInfo: "",
+      showPokedex: false,
+    };
 
-        // console.log(this.state)
+    // console.log(this.state)
+  }
+
+  componentDidMount = () => {
+    // console.log("FIRED! componentDidMount for "+this.state.name)
+
+    let url = this.state.url;
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((pokeData) => {
+        let pokeInfo = this.state.pokeInfo;
+        pokeInfo = pokeData;
+        this.setState({ pokeInfo });
+
+        let imgsrc = this.state.pokeInfo.sprites.other.dream_world
+          .front_default;
+        this.setState({ img: imgsrc });
+      });
+  };
+
+  handlePokedex = () => {
+    console.log("FIRED! handlePokedex for " + this.state.pokeInfo.name);
+    console.log(this.state.pokeInfo, this.state.showPokedex);
+    // console.log(this.state.pokeInfo.sprites.other.official-artwork.front_default)
+
+    // THE GREAT POKEMON INFORMATION TRAWLER
+
+    let pokeNAME = this.state.pokeInfo.name;
+    let statNUM = this.state.pokeInfo.id;
+    let statXP = this.state.pokeInfo.base_experience;
+    let statHP = this.state.pokeInfo.stats[0].base_stat;
+    let statATK = this.state.pokeInfo.stats[1].base_stat;
+    let statDEF = this.state.pokeInfo.stats[2].base_stat;
+    let statSPD = this.state.pokeInfo.stats[5].base_stat;
+    let statWeight = this.state.pokeInfo.weight;
+    let statABL = this.state.pokeInfo.abilities;
+
+    let type1 = this.state.pokeInfo.types[0].type.name;
+    let typeArray = this.state.pokeInfo.types;
+    let length = typeArray.length;
+    let types = "";
+
+    if (length > 1) {
+      let type2 = this.state.pokeInfo.types[1].type.name;
+      types += "Types: ";
+      types += type1;
+      types += ", ";
+      types += type2;
+    } else {
+      types += "Type: ";
+      types += type1;
     }
+    console.log(types);
 
-    componentDidMount = () => {
-        // console.log("FIRED! componentDidMount for "+this.state.name)
-
-        let url = this.state.url
-
-        fetch(url)
-        .then(response => response.json())
-        .then((pokeData) => {
-            let pokeInfo = this.state.pokeInfo
-            pokeInfo = pokeData
-            this.setState({pokeInfo})
-
-            let imgsrc = this.state.pokeInfo.sprites.other.dream_world.front_default
-            this.setState({img: imgsrc})
-        })
-
-        
-    }    
-
-    handlePokedex = () => {
-        console.log("FIRED! handlePokedex for "+this.state.pokeInfo.name)
-        console.log(this.state.pokeInfo, this.state.showPokedex)
-        // console.log(this.state.pokeInfo.sprites.other.official-artwork.front_default)
-
-        
-        // THE GREAT POKEMON INFORMATION TRAWLER
-
-
-        let statXP = this.state.pokeInfo.base_experience
-        let statHP = this.state.pokeInfo.stats[0].base_stat
-        let statATK = this.state.pokeInfo.stats[1].base_stat
-        let statDEF = this.state.pokeInfo.stats[2].base_stat
-        let statSPD = this.state.pokeInfo.stats[5].base_stat
-        let statWeight = this.state.pokeInfo.weight
-
-        let type1 = this.state.pokeInfo.types[0].type.name
-        let typeArray = this.state.pokeInfo.types
-        let length = typeArray.length
-        let types = ''
-
-        if (length > 1) {
-            let type2 = this.state.pokeInfo.types[1].type.name
-            types += "Types: "
-            types += type1
-            types +=", "
-            types += type2
-        }
-        else {
-            types += "Type: "
-            types += type1
-        }
-        console.log(types)
-
-        this.setState({types: types})
-        this.setState({XP: statXP})
-        this.setState({HP: statHP})
-        this.setState({ATK: statATK})
-        this.setState({DEF: statDEF})
-        this.setState({SPD: statSPD})
-        this.setState({WEI: statWeight})
-
-        if (!this.state.showPokedex) {
-            let bool = true
-            this.setState({showPokedex: bool})
-        }
-        else {
-            let bool = false
-            this.setState({showPokedex: bool})
-        }
-
+    let abilities = "Abilities: ";
+    let i = 0;
+    for (i = 0; i < statABL.length; i++) {
+      let ability = statABL[i].ability.name;
+      ability = ability.charAt(0).toUpperCase() + ability.slice(1);
+      abilities = abilities + ability;
+      if (i < statABL.length - 1) {
+        abilities = abilities + ", ";
+      }
     }
+    console.log(abilities);
 
-    render = () => {
-        let pokedex
-        let show = this.state.showPokedex
-        let backgroundStyle = {backgroundImage:"url("+this.state.img+")", backgroundSize:"contain", backgroundPosition:"center center", backgroundRepeat:"no-repeat"}
+    this.setState({ NAME: pokeNAME });
 
-        if (show) {
-            pokedex = 
-                <div className="pokedex" style={{zIndex:"100"}}>
-                    <h1 style={{textTransform:"capitalize"}}>{this.state.pokeInfo.name}</h1>
-                    <img src={this.state.img} height="300px"></img>
-                    <h2 style={{textTransform:"capitalize"}}>{this.state.types}</h2>
-                    <div className="statFlex">
-                        <h3 className="stat">    
-                            Base XP: {this.state.XP}
-                        </h3>
-                        <h3 className="stat">  
-                            HP: {this.state.HP}
-                        </h3>
-                        <h3 className="stat">                             
-                            Atack: {this.state.ATK}
-                        </h3>
-                        <h3 className="stat">                             
-                            Defense: {this.state.DEF}
-                        </h3>
-                        <h3 className="stat">                             
-                            Speed: {this.state.SPD}
-                        </h3>
-                        <h3 className="stat">                             
-                            Weight: {this.state.WEI}
-                        </h3>
-                    </div>
-                </div>
-                
-        }
-        else {
-            pokedex = <div style={{display:'none'}}></div>
-        }
+    this.setState({ types: types });
+    this.setState({ NUM: statNUM });
+    this.setState({ XP: statXP });
+    this.setState({ HP: statHP });
+    this.setState({ ATK: statATK });
+    this.setState({ DEF: statDEF });
+    this.setState({ SPD: statSPD });
+    this.setState({ WEI: statWeight });
+    this.setState({ ABL: abilities });
 
-        return(
-            <div className="pokeContainerDoNotTouch" onClick={this.handlePokedex}>
- 
-                <div className="pokedexContainer">
-                    {pokedex}
-                </div>
+    if (!this.state.showPokedex) {
+      let bool = true;
+      this.setState({ showPokedex: bool });
+    } else {
+      let bool = false;
+      this.setState({ showPokedex: bool });
+    }
+  };
 
-                <div className="container" style={backgroundStyle}>
-                    <h3 className="pokeTitle" style={{zIndex:"-1", textTransform:"capitalize"}}>{this.props.name}</h3>
-                </div>
+  render = () => {
+    let pokedex;
+    let show = this.state.showPokedex;
+    let backgroundStyle = {
+      backgroundImage: "url(" + this.state.img + ")",
+      backgroundSize: "contain",
+      backgroundPosition: "center center",
+      backgroundRepeat: "no-repeat",
+    };
 
-                
-
+    if (show) {
+      pokedex = (
+        <div className="pokedex" style={{ zIndex: "100" }}>
+          <h1 className="pokeName" style={{ textTransform: "capitalize" }}>
+            {this.state.NAME}
+          </h1>
+          <div className="pokeBox">
+            <h1 height="69px"></h1>
+            <img src={this.state.img} className="pokeImg"></img>
+            <h2 style={{ textTransform: "capitalize" }}>{this.state.types}</h2>
+            <h3 className="">{this.state.ABL}</h3>
+            <div className="statFlex">
+              <h3 className="stat">
+                Base XP: <br /> {this.state.XP}
+              </h3>
+              <h3 className="stat">
+                HP: <br /> {this.state.HP}
+              </h3>
+              <h3 className="stat">
+                Atack: <br /> {this.state.ATK}
+              </h3>
+              <h3 className="stat">
+                Defense: <br /> {this.state.DEF}
+              </h3>
+              <h3 className="stat">
+                Speed:
+                <br /> {this.state.SPD}
+              </h3>
+              <h3 className="stat">
+                Weight: <br /> {this.state.WEI}
+              </h3>
             </div>
-        )
+          </div>
+          <div className="pokeNum">
+            <h1>#{this.state.NUM}</h1>
+          </div>
+          <div className="pokeExit" onClick={this.handlePokedex}>
+            <img
+              src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c23eacf8-e366-4118-ab5c-5a1255842a75/de44dk0-f367f584-c0ea-46aa-9675-46eeb964636e.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOiIsImlzcyI6InVybjphcHA6Iiwib2JqIjpbW3sicGF0aCI6IlwvZlwvYzIzZWFjZjgtZTM2Ni00MTE4LWFiNWMtNWExMjU1ODQyYTc1XC9kZTQ0ZGswLWYzNjdmNTg0LWMwZWEtNDZhYS05Njc1LTQ2ZWViOTY0NjM2ZS5wbmcifV1dLCJhdWQiOlsidXJuOnNlcnZpY2U6ZmlsZS5kb3dubG9hZCJdfQ.kFME8zKls7_x2mEptLNoEMO6z57Oy7dhCeW7goKPSfM"
+              alt="Menu Button"
+              width="50px"
+              height="50px"
+            ></img>
+          </div>
+        </div>
+      );
+    } else {
+      pokedex = <div style={{ display: "none" }}></div>;
     }
+
+    return (
+      <div className="pokeContainerDoNotTouch">
+        <div className="pokedexContainer">{pokedex}</div>
+
+        <div
+          className="container"
+          style={backgroundStyle}
+          onClick={this.handlePokedex}
+        >
+          <h3
+            className="pokeTitle"
+            style={{ zIndex: "-1", textTransform: "capitalize" }}
+          >
+            {this.props.name}
+          </h3>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default Pokemon;
